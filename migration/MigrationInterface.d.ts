5a9bd4a186b15afab7f93edcc8d5f79053856404
Mac Lockard <maclockard@gmail.com>
2025-03-10 17:59:45 -0600

    Publish

---
+++ migration/MigrationInterface.d.ts
@@ -0,0 +1,24 @@
+import { QueryRunner } from "../query-runner/QueryRunner";
+/**
+ * Migrations should implement this interface and all its methods.
+ */
+export interface MigrationInterface {
+    /**
+     * Optional migration name, defaults to class name.
+     */
+    name?: string;
+    /**
+     * If true, run this migration inside of a transaction.
+     *
+     * @default true
+     */
+    transaction?: boolean;
+    /**
+     * Run the migrations.
+     */
+    up(queryRunner: QueryRunner): Promise<any>;
+    /**
+     * Reverse the migrations.
+     */
+    down(queryRunner: QueryRunner): Promise<any>;
+}
