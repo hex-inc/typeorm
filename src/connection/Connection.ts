3478222669a61663d681dda1a148bc3152b191ee
Mac Lockard <maclockard@gmail.com>
2025-03-11 18:17:31 -0600

    Query tagging tweaks

--- src/connection/Connection.ts
+++ src/connection/Connection.ts
@@ -450,7 +450,10 @@ export class Connection {
     createQueryRunner(mode: ReplicationMode = "master"): QueryRunner {
         const queryRunner = this.driver.createQueryRunner(mode);
         if (queryRunner.setMetadata != null && this.options.getMetadata != null) {
-          queryRunner.setMetadata(this.options.getMetadata());
+            const metadata = this.options.getMetadata();
+            if (metadata != null) {
+                queryRunner.setMetadata(metadata);
+            }
         }
         const manager = this.createEntityManager(queryRunner);
         Object.assign(queryRunner, { manager: manager });
